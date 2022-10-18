9783350825662f61f0ae049af3514216d85aff4d
Ultra119 <pasasvistak0@gmail.com>
2022-10-18 21:31:18 +0300

    rewrite with pathlib
    also added auto zip or rar unpacking and installing
    
    Co-Authored-By: volfram <ihapakaz@gmail.com>

--- main.py
+++ main.py
@@ -1,65 +1,59 @@
-# Stellaris Mods Installer v1.1 by Ultra119
+# Stellaris Mods Installer v1.2 by Ultra119
 
-import os
 import shutil
 
+from pathlib import Path
 
-# Get the path to the folder from which the script was launched
-root_path = os.getcwd()
+def get_root_path():
+    return Path.cwd()
 
-# Get a list of all folders in the root
-root_folders = os.listdir(root_path)
+def extract_mod(file_path: Path):
+    path_to_copy = get_root_path() / (file_path.parent.name + '.mod')
+    shutil.copy(str(file_path), str(path_to_copy))
 
-# For each folder in the root
-for mod_folder_name in root_folders:
-    # Get the full path to the folder
-    mod_folder_path = os.path.join(root_path, mod_folder_name)
+    with path_to_copy.open("r+") as mod_file:
+        # Delete the line that starts with path=, if it is present in the file
+        lines = mod_file.readlines()
+        mod_file.seek(0)
+        for line in lines:
+            if not line.startswith("path="):
+                mod_file.write(line)
+        mod_file.truncate()
 
-    # If the folder is a directory
-    if os.path.isdir(mod_folder_path):
-        mod_files = os.listdir(mod_folder_path)
+        # Add a new line at the end of the file with the full path from which the file was copied
+        mod_file.write("\n" + "path=" + '"' + str(file_path.parent) + '"' + "\n")
 
-        for file in mod_files:
-            file_path = os.path.join(mod_folder_path, file)
-            file_ext = os.path.splitext(file_path)[1]
+    print("INFO: " + str(file_path.parent) + " installed!")
 
-            # If the file extension is .mod
-            if file_ext == ".mod":
-                if os.path.exists(os.path.join(root_path, mod_folder_name + ".mod")):
-                    print("INFO: " + mod_folder_path + ".mod already exists")
-                    break
-                else:
-                    # Copy the file to the folder in which the script is located
-                    shutil.copy(file_path, root_path)
+def main():
+    root_path = get_root_path()
+    for mod_folder_path in root_path.iterdir():
+        path_file_mod = root_path / (mod_folder_path.name + '.mod')
+        if path_file_mod.exists():
+            print("INFO: " + str(path_file_mod) + " already exists")
+            continue
+        else:
+            if mod_folder_path.is_dir():
+                # print(mod_folder_path)
+                files = [f for f in mod_folder_path.iterdir() if f.is_file()]
+                for file in files:
+                    if file.suffix == '.mod':
+                        extract_mod(file)
+                    elif file.suffix in ('.zip', '.rar'):
+                        print("INFO: " + str(file) + " is not a .mod, unpacking...")
+                        shutil.unpack_archive(str(file), str(mod_folder_path))
+                        files_new = [f for f in mod_folder_path.iterdir() if f.is_file() and f.suffix == '.mod']
+                        for file_unpacked in files_new:
+                            extract_mod(file_unpacked)
 
-                # Rename the copied file to the name of the folder from which it was originally copied
-                shutil.move(os.path.join(root_path, file), os.path.join(root_path, mod_folder_name + ".mod"))
+            elif mod_folder_path.is_file():
+                if mod_folder_path.suffix in ('.zip', '.rar'):
+                    print("INFO: " + str(mod_folder_path) + " is not a .mod, unpacking...")
+                    shutil.unpack_archive(str(mod_folder_path), str(root_path / mod_folder_path.stem))
+                    files_new = [f for f in (root_path / mod_folder_path.stem).iterdir() if f.is_file() and f.suffix == '.mod']
+                    for file_unpacked in files_new:
+                        extract_mod(file_unpacked)
 
-                # Get the full path to the renamed file
-                renamed_file_path = os.path.join(root_path, mod_folder_name + ".mod")
-                with open(renamed_file_path, "r+") as mod_file:
-                    # Delete the line that starts with path=, if it is present in the file
-                    lines = mod_file.readlines()
-                    mod_file.seek(0)
-                    for line in lines:
-                        if not line.startswith("path="):
-                            mod_file.write(line)
-                    mod_file.truncate()
-
-                    # Add a new line at the end of the file with the full path from which the file was copied
-                    mod_file.write("\n" + "path=" + '"' + mod_folder_path + '"' + "\n")
-
-                print("INFO: " + mod_folder_path + " installed!")
-
-            elif file_ext == ".zip" and ".rar":
-                print("ERROR: " + file_path + " is not a .mod, unpack it and run script again")
-                break
-
-            else:
-                pass
-
-    else:
-        pass
+main()
 
 input("Press Enter to exit...")
-exit()
